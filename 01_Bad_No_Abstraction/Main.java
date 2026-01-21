7f70873a6a9b9dbee3eed0909592c64d7a77db8d
kaungzawhein <kaungzawhein@onenex.co>
2026-01-21 19:01:08 +0700

    update abstraction codes

--- 01_Bad_No_Abstraction/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Cash cashPayment = new Cash();
-        cashPayment.pay(10.0);
-
-        CreditCard creditCardPayment = new CreditCard();
-        creditCardPayment.pay(25.0);
-    }
-}
