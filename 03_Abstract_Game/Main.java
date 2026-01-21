99992214fccc6e57d917b27112f18d46a782d086
kaungzawhein <kaungzawhein@onenex.co>
2026-01-21 15:19:33 +0700

    update abstraction codes

--- 03_Abstract_Game/Main.java
+++ 03_Abstract_Game/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         
-        GameCharacter warrior = new Warrior("Aragorn");
+        GameCharacter warrior = new Warrior("Warrior");
         warrior.walk();   
         warrior.attack(); 
 
         System.out.println(); // for spacing
 
         
-        GameCharacter mage = new Mage("Gandalf");
+        GameCharacter mage = new Mage("Mage");
         mage.walk();   
         mage.attack(); 
     }
